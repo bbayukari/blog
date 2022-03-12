16413a80cf9d1d18c904bf674d961416ae6769bf
bbayukari <40588568+bbayukari@users.noreply.github.com>
2022-03-12 11:56:03 +0800

    Update example1.cpp

--- example_of_optim/example1.cpp
+++ example_of_optim/example1.cpp
@@ -1,14 +1,15 @@
-#define OPTIM_ENABLE_EIGEN_WRAPPERS // it means that we'll use the eigen lib to compute
-#include "optim.hpp" // it means we'll use this optim lib
+#define OPTIM_ENABLE_EIGEN_WRAPPERS // it means that we'll use the eigen lib to compute matrix
+#include "optim.hpp" // it means that we'll use this optim lib
 
 inline // cpp grammar, it's not necessary
-double sphere_fn(const Eigen::VectorXd& vals_inp, Eigen::VectorXd* grad_out, void* opt_data)
-{ // this function is object function which can compute both function value and its differential
+double sphere_fn(const Eigen::VectorXd& vals_inp, Eigen::VectorXd* grad_out, void* opt_data){
+  // this function is object function which can compute both function value and its gradient
   // and it is to be provided to the algorithm function later
-    double obj_val = vals_inp.dot(vals_inp); // obj_val is to be function value
+  
+    double obj_val = vals_inp.dot(vals_inp); // obj_val is function value
 
-    if (grad_out) { // if grad_out != NULL, we should compute differential, otherwise we needn't
-        *grad_out = 2.0*vals_inp; // this is the differntial 
+    if (grad_out) { // if grad_out != NULL, we should compute gradient, otherwise we needn't
+        *grad_out = 2.0*vals_inp; // this is the gradient 
     }
 
     return obj_val; // return function value
@@ -20,7 +21,7 @@ int main()
 
     Eigen::VectorXd x = Eigen::VectorXd::Ones(test_dim); // initial values (1,1,...,1)
 
-    optim::algo_settings_t settings; // this is to set para for algorithm
+    optim::algo_settings_t settings; // this is used to set parameters for algorithm
 
     settings.print_level = 4; // it means that all information will be printed
 
@@ -28,11 +29,10 @@ int main()
 
     if (success) {
         std::cout << "gd: sphere test completed successfully." << "\n";
+        std::cout << "gd: solution to sphere test:\n" << x << std::endl;
     } else {
         std::cout << "gd: sphere test completed unsuccessfully." << "\n";
     }
-
-    std::cout << "gd: solution to sphere test:\n" << x << std::endl;
 
     return 0;
 }
